b21b7f01a941618537de8dae5254f2b03a437162
salim923 <ferchichisalim599@gmail.com>
2025-02-18 17:33:06 +0100

    changes

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public  string Username { get; set; }  // Ensure uniqueness in the DB
         public  string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
     }
 }
